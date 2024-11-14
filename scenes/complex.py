9ccd89eecef2336ed73a0c6cea9305cf18e31c14
Jul1xn <88789943+jul15xn@users.noreply.github.com>
2024-11-14 12:16:16 +0100

    a

--- scenes/complex.py
+++ scenes/complex.py
@@ -6,4 +6,9 @@ class Complex(Scene):
         super().__init__()
 
     def intro(self):
-        dlo.print_text_dialogue("")
+        dlo.print_text_dialogue("Je bent nu in het cellencomplex.<600> Je zit heel veel cellen, <300>en aan het eind een bewaker.<600>", 2)
+        dlo.new_line()
+        dlo.print_text_dialogue("Ook zie je een opvallende man die opvallend naar je kijkt.<600>", 1.3)
+        dlo.new_line()
+        dlo.print_text_dialogue("Wat wil je doen?<400>", 0.5)
+        dlo.new_line()
